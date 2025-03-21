2c396c82e59daede71e13ceb098f62844951500c
MaxanceV <maxance.villame@gmail.com>
2025-03-21 14:17:42 +0100

    First commit

---
+++ src/test/java/com/dauphine/blogger/box/backend/BloggerBoxBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dauphine.blogger.box.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BloggerBoxBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
